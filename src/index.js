2c5d3213fec7638981269e6ea1b006e2089569f7
Dania Diar <daniadeyar@gmail.com>
2022-08-07 00:11:34 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+
+  taskForm.addEventListener("submit", createTask);
 });
+
+const createTask = (event) => {
+  event.preventDefault();
+
+  const taskDescription = document.getElementById("new-task-description");
+  const task = document.createElement("li");
+  task.innerText = taskDescription.value;
+
+  document.getElementById("tasks").appendChild(task);
+  event.target.reset();
+};
